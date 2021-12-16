3f6b6eb7605b2a18b2db98073ca8cc71a91021c8
Estelle Ariane <estellesandjong@gmail.com>
2021-12-16 16:42:18 -0500

    push

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -8,7 +8,7 @@ spending_categories = []
 for expense in expenses.list:
      spending_categories.append(expense.category)
 
-spending_counter = collections.counter(spending_categories)
+spending_counter = collections.Counter('spending_categories')
 print(spending_counter)
 top5 = spending_counter.most_common(5)
 categories, count = zip(*top5)
