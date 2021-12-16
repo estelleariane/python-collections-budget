6c51e9f8cd7bec1db05ac2b17ccdaccb1ad32096
Estelle Ariane <estellesandjong@gmail.com>
2021-12-16 12:34:27 -0500

    push

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -2,8 +2,6 @@ from . import Expense
 import collections
 import matplotlib.pyplot as plt
 
-
-
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 spending_categories = []
