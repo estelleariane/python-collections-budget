28ad42d2f98af7f416cd713f64bffe156de7edff
Estelle Ariane <estellesandjong@gmail.com>
2021-12-18 09:24:01 -0500

    push

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -3,16 +3,17 @@
 import budget
 
 
-class BudgetList
-def_init_(self, budget)
-self.budget = budget
-self.sum_expenses = 0
-self.expenses = []
-self.sum_overages = 0
-self.overages = []
-list.append( self, item)
-if self.sum_expenses + item < self.budget
-self.expenses.append(item)
+class BudgetList: 
+ def _init_(self, budget):
+  self.budget = budget
+  self.sum_expenses = 0
+  self.expenses = []
+  self.sum_overages = 0
+  self.overages = []
+BudgetList.append( item)
+if self.sum_expenses + item < self.budget:
+ self.expenses.append(item)
 item+ self.sum_expenses
-else self.overages.append(item)
+else 
+self.overages.append(item)
 self.sum_overages = self.sum_overages+item
